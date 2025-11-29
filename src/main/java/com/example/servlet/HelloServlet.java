1814838c56d6611269748b6be2a1e90f732e2301
HK <hk@gmail.com>
2025-11-29 22:49:54 +0530

    Commiting JAVA Application

---
+++ src/main/java/com/example/servlet/HelloServlet.java
@@ -0,0 +1,16 @@
+package com.example.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Employee WebApp - Jenkins CI/CD Practice</h1>");
+        response.getWriter().println("<p>Welcome Hari! This is your Java application deployment project.</p>");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Employee WebApp</title>
+</head>
+<body>
+<h2>Welcome to Employee WebApp</h2>
+<p>This is a simple Java Web Project for Jenkins CI/CD practice.</p>
+<p><a href="hello">Click to run servlet</a></p>
+</body>
+</html>
